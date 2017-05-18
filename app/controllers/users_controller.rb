adfa0132184099aa6814eec0330df58c62792971
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-05-18 12:21:27 -0400

    modified usercontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,14 +15,18 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+      @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+      @user.name = params[:name]
+      @user.password = params[:password]
+      @user.money = 10000
+
+      if @user.save
+        render json: @user, status: :created, location: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
-  end
 
   # PATCH/PUT /users/1
   def update
