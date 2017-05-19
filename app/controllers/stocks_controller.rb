3fc21e55f8199f04f9ad2c0fd29792ea111b6c52
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-05-18 21:59:27 -0400

    Add ticker column in the Stock table

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -38,6 +38,8 @@ class StocksController < ApplicationController
     @stock.destroy
   end
 
+  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_stock
