9b652047779bb86549fc9b342cf42566b2d47e6c
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-05-21 22:58:04 -0400

    Buy a stock

--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -4,7 +4,6 @@ class LedgersController < ApplicationController
   # GET /ledgers
   def index
     @ledgers = Ledger.all
-
     render json: @ledgers
   end
 
@@ -16,13 +15,121 @@ class LedgersController < ApplicationController
   # POST /ledgers
   def create
     @ledger = Ledger.new(ledger_params)
+    stockprice = 0
+    # Get current Stock
+    stock = params[:stock]
+    userId = params[:user][:id]
 
-    if @ledger.save
-      render json: @ledger, status: :created, location: @ledger
-    else
-      render json: @ledger.errors, status: :unprocessable_entity
+    # currentUser
+    @user = User.find(userId)
+    isWatched = params[:isWatched]
+
+    # if the stock is not watched - mean user is buying a stock.
+    # if user buys a stock. Need to set USER_ID, QTY, PRICE, Watch= false and a stock symbol
+    @ledger.user_id = userId
+    @ledger.symbol = stock[:symbol]
+    @ledger.watched = isWatched
+
+    if !isWatched
+      @userBuyQty = params[:qty]
+
+      if @userBuyQty  < 1
+        render json: {errors: "QTY should not be blank."}
+      end
+
+      @userFund = @user[:money]
+      if stock[:ask]
+        @stockprice = stock[:ask]
+      else
+        @stockprice =  stock[:last_trade_price_only]
+      end
+
+      @subSum = @stockprice * @userBuyQty
+      if @userFund.to_f.round(2) >= @subSum.to_f.round(2)
+        puts "enough money...."
+
+        # need to subtract $ from user fund..
+        minusMoney = @userFund.to_f.round(2) - @subSum.to_f.round(2)
+        @user[:money] = minusMoney
+
+        # find user stock, with id, symbol, watched = false .. if there is a same stock that user is planning to buy.  Need to add the Order # and money and divide to get the aveage $ which user buys.
+        tempstockledger = Ledger.where(user_id: @user[:id], symbol: stock[:symbol], watched: false)
+
+        if tempstockledger.size > 0
+          # Update stock
+          tempstockledger.each do |x|
+            @tempQty = x.qty
+            @tempPrice = x.price
+            sumTempQtyPrice = @tempQty * @tempPrice
+            @weightedAveragePrice = (sumTempQtyPrice + @subSum) / (@tempQty + @userBuyQty)
+            @totalShares = @tempQty + @userBuyQty
+
+            # x.price = @weightedAveragePrice.to_f.round(2)
+            # x.qty = @totalShares
+          end
+
+          Ledger.where(user_id: @user[:id], symbol: @ledger.symbol , watched: false).update_all(price: @weightedAveragePrice.to_f.round(2), qty: @totalShares)
+
+          @userStocks = User.find(@user[:id]).ledgers
+             #  save User too.
+            @user.save!
+
+              render json: {
+                  userstocks: @userStocks,
+                  currentUser: @user
+               }
+
+        else
+          puts "I am in else if there is no duplicate"
+          # save new Stock
+          @ledger.price = @stockprice
+          @ledger.qty = @userBuyQty
+
+          p "@ledger.price" , @ledger.price.to_f.round(2)
+          p "@ledger.qty" , @ledger.qty
+
+          if @ledger.save
+             @user.save!
+             @userStocks = User.find(@user[:id]).ledgers
+            #  save User too.
+             render json: {
+              userstocks: @userStocks,
+              currentUser: @user
+            }
+          else
+            render json: @ledger.errors, status: :unprocessable_entity
+          end
+
+        end
+
+
+      else
+        render json: {errors: "Not Enough Fund"}
+      end
+
     end
+    # if you get this fall you are ok ..
+    # new - ledger
+  else
+    # @ledger.price = 0
+    # @ledger.qty = 0
+    #
+    #
+    #   if @ledger.save
+    #     @user.save!
+    #      @userStocks = User.find(@user[:id]).ledgers
+    #     #  save User too.
+    #      render json: {
+    #        userstocks: @userStocks,
+    #        currentUser: @user
+    #     }
+      # else
+      #   render json: @ledger.errors, status: :unprocessable_entity
+      # end
   end
+
+
+
 
   # PATCH/PUT /ledgers/1
   def update
@@ -46,6 +153,6 @@ class LedgersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def ledger_params
-      params.require(:ledger).permit(:user_id, :stock_id, :qty)
+      params.require(:ledger).permit(:user_id, :symbol, :qty)
     end
 end
